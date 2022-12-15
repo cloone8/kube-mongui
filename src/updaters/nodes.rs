00e5577c68faa72865e8e8fc9b80f42241a509e7
Wouter de Bruijn <wouter@debruijnonline.nl>
2022-12-16 00:17:05 +0100

    Retrieving metrics and logging for API requests

--- src/updaters/nodes.rs
+++ src/updaters/nodes.rs
@@ -1,10 +1,13 @@
 use std::{thread::{self, sleep}, time::Duration};
 
-use k8s_openapi::{api::core::v1::Node, ListResponse};
+use k8s_metrics::v1beta1::NodeMetrics;
+use k8s_openapi::{api::core::v1::Node, ListResponse, List};
 
 use crate::{util::request_util, data::node::{NodeInfo, OsDetail}};
 
-fn get_node_info(node: &Node) -> NodeInfo {
+fn get_node_info(node: &Node, _: Option<&NodeMetrics>) -> NodeInfo {
+
+    // TODO: Clean up this disgusting mess
     let node_name = node.metadata.name.clone().unwrap();
     let arch = node.status.as_ref().unwrap().node_info.as_ref().unwrap().architecture.clone();
 
@@ -13,6 +16,8 @@ fn get_node_info(node: &Node) -> NodeInfo {
         kernel_version: Some(node.status.as_ref().unwrap().node_info.as_ref().unwrap().kernel_version.clone()),
         image: Some(node.status.as_ref().unwrap().node_info.as_ref().unwrap().os_image.clone()),
     };
+
+    // END TODO
 
     NodeInfo {
         name: node_name,
@@ -23,24 +28,48 @@ fn get_node_info(node: &Node) -> NodeInfo {
     }
 }
 
+fn get_node_metric_pair<'a>(node: &'a Node, metrics: &'a Vec<NodeMetrics>) -> (&'a Node, Option<&'a NodeMetrics>) {
+    for metric in metrics {
+        if metric.metadata.name == node.metadata.name {
+            return (node, Some(metric));
+        }
+    }
+
+    log::debug!("No metrics found for node {}", node.metadata.name.as_ref().unwrap());
+
+    (node, None)
+}
+
+fn get_new_nodes(node_response: List<Node>, metrics_response: Vec<NodeMetrics>) -> Vec<NodeInfo> {
+    node_response.items.iter()
+        .map(|node| get_node_metric_pair(node, &metrics_response))
+        .map(|pair| get_node_info(pair.0, pair.1))
+        .collect()
+}
+
 pub(crate) fn start(ui_info: &mut crate::KubeMonGUI) -> Result<(), ()> {
     let ip = ui_info.proxy.listen_addr.ip();
     let port = ui_info.proxy.listen_addr.port();
+
     let url = format!("http://{}:{}/api/v1/nodes", ip, port);
+    let url_metrics = format!("http://{}:{}/apis/metrics.k8s.io/v1beta1/nodes", ip, port);
 
     let nodes = ui_info.nodes.clone();
 
     thread::spawn(move || loop {
         let response = request_util::get_response_from_url::<ListResponse<Node>>(url.as_str());
+        let metrics_response = request_util::attempt_as_json::<NodeMetrics>(url_metrics.as_str());
 
         if let Ok(ListResponse::Ok(response)) = response {
-            let new_nodes: Vec<NodeInfo> = response.items.iter()
-                .map(get_node_info)
-                .collect();
+            if let Ok(metrics_response) = metrics_response {
 
-            let mut nodes_locked = nodes.lock();
-            nodes_locked.clear();
-            nodes_locked.extend(new_nodes);
+                let new_nodes: Vec<NodeInfo> = get_new_nodes(response, metrics_response);
+
+                let mut nodes_locked = nodes.lock();
+
+                nodes_locked.clear();
+                nodes_locked.extend(new_nodes);
+            }
         }
 
         sleep(Duration::from_secs(1));
--- src/util/request_util.rs
+++ src/util/request_util.rs
@@ -6,6 +6,23 @@ pub enum RequestError {
 pub fn get_response_from_url<T>(url: &str) -> Result<T, RequestError>
     where T: k8s_openapi::Response
 {
+    log::info!("Requesting (typed): {}", url);
+
+    let response = get_response_from_url_unlogged::<T>(url);
+
+    if response.is_err() {
+        log::warn!("Request failed: {}", url);
+    } else {
+        log::debug!("Request succeeded: {}", url);
+    }
+
+    response
+}
+
+#[inline]
+fn get_response_from_url_unlogged<T>(url: &str) -> Result<T, RequestError>
+    where T: k8s_openapi::Response
+{
     let response = match reqwest::blocking::get(url) {
         Ok(it) => it,
         Err(err) => return Err(RequestError::Request(err)),
@@ -15,4 +32,39 @@ pub fn get_response_from_url<T>(url: &str) -> Result<T, RequestError>
         Ok((parsed, _)) => Ok(parsed),
         Err(parse_err) => Err(RequestError::Parse(parse_err)),
     }
+}
+
+pub fn attempt_as_json<T>(url: &str) -> Result<Vec<T>, RequestError>
+    where T: serde::de::DeserializeOwned
+{
+    log::info!("Requesting (json): {}", url);
+
+    let response = attempt_as_json_unlogged::<T>(url);
+
+    if response.is_err() {
+        log::warn!("Request failed: {}", url);
+    } else {
+        log::debug!("Request succeeded: {}", url);
+    }
+
+    response
+}
+
+#[inline]
+fn attempt_as_json_unlogged<T>(url: &str) -> Result<Vec<T>, RequestError>
+    where T: serde::de::DeserializeOwned
+{
+    let response = match reqwest::blocking::get(url) {
+        Ok(it) => match it.json::<serde_json::Value>() {
+            Ok(json) => json,
+            Err(err) => return Err(RequestError::Request(err)),
+        }
+        Err(err) => return Err(RequestError::Request(err)),
+    };
+
+    let items = response["items"].as_array().unwrap().iter()
+        .map(|item| serde_json::from_value::<T>(item.clone()).unwrap())
+        .collect::<Vec<T>>();
+
+    Ok(items)
 }
